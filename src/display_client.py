067aa67839769bb175fc1065c8565716022b8355
Lazlo <local.admin@localhost.com>
2020-08-03 22:26:44 -0400

    Why is this not committing?

--- src/display_client.py
+++ src/display_client.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import time
 
 RST = 24
@@ -10,18 +12,30 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 
 def on_message(client, userdata, message):
+    global display
+    global draw
+    global font
+    global image
     clean_msg = str(message.payload.decode('utf-8')).split(' ')
+    print(clean_msg)
     command = clean_msg[0]
-    if command is "camera":
+    if command == "camera":
         display.clear()
-        display.image()
-    elif command is "time":
+        image = Image.open('icons/camera_icon.ppm').resize((128,64), Image.ANTIALIAS).convert('1')
+        display.image(image)
+        display.display()
+        image = Image.new('1', (128, 64))
+    elif command == "time":
         display.clear()
-        display.text(
+        image = Image.new('1', (128, 64))
+        draw.text((0,0),clean_msg[1],font=font, fill=255)
+        display.image(image)
+        display.display()
     
-        
-
 display = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+font = ImageFont.load_default()
+image = Image.new('1', (128, 64))
+draw = ImageDraw.Draw(image)
 
 display.begin()
 display.clear()
@@ -30,11 +44,6 @@ display.display()
 client = mqtt.Client("display")
 client.connect('127.0.0.1',port=1883,keepalive=60)
 client.subscribe('display')
-
-image = Image.open('icons/camera_icon.ppm').resize((128,64), Image.ANTIALIAS).convert('1')
-
-display.image(image)
-display.display()
 
 client.on_message=on_message
 
