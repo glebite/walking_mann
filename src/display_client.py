221e6c93abd90e837dcee5ca46b266ee84fa5be6
Lazlo <glebite@gmail.com>
2020-08-24 21:59:31 -0400

    Yup - more pylint3 cleanup.

--- src/display_client.py
+++ src/display_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-display_client.py - 
+display_client.py - uses mqtt for messaging and relaying info to the 
+                    display
 """
 
 import paho.mqtt.client as mqtt
@@ -12,13 +13,13 @@ from PIL import ImageFont
 import time
 import logging
 
-logger = logging.getLogger('display_client')
-logger.setLevel(logging.DEBUG)
+LOGGER = logging.getLOGGER('display_client')
+LOGGER.setLevel(logging.DEBUG)
 FH = logging.FileHandler('display_client.log')
 FH.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-FH.setFormatter(formatter)
-logger.addHandler(FH)
+FORMATTER = logging.FORMATTER('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+FH.setFORMATTER(FORMATTER)
+LOGGER.addHandler(FH)
 
 class Display:
     """
@@ -28,19 +29,19 @@ class Display:
         """
         __init__
         """
-        logger.debug('initializing...')
+        LOGGER.debug('initializing...')
         self.RESET = 24
         self.DC = 23
         self.SPI_PORT = 0
         self.SPI_DEVICE = 0
 
-        logger.debug('creating/connecting MQTT system')
+        LOGGER.debug('creating/connecting MQTT system')
         self.client = mqtt.Client("display")
         self.client.connect("127.0.0.1", port=1883, keepalive=60)
         self.client.subscribe("display")
         self.client.on_message = self.on_message
 
-        logger.debug('setting up display')
+        LOGGER.debug('setting up display')
         self.display = Adafruit_SSD1306.SSD1306_128_64(rst=self.RESET)
         self.font = ImageFont.truetype('/home/pi/VCR_OSD_MONO_1.001.ttf', 48)
         self.display.begin()
@@ -49,7 +50,7 @@ class Display:
         """
         loop
         """
-        logger.debug('entering loop')
+        LOGGER.debug('entering loop')
         self.client.loop_start()
         while True:
             time.sleep(0.1)
@@ -60,41 +61,41 @@ class Display:
         on_message
         """
         clean_msg = str(message.payload.decode('utf-8')).split(' ')
-        logger.debug('clean message is: {}'.format(clean_msg))
+        LOGGER.debug('clean message is: {}'.format(clean_msg))
         command = clean_msg[0]
         if command == "camera":
-            logger.debug("Handling camera")
+            LOGGER.debug("Handling camera")
             return_code = self.display.clear()
-            logger.debug('display.clear rc: {}'.format(return_code))
+            LOGGER.debug('display.clear rc: {}'.format(return_code))
             return_code = self.display.display()
-            logger.debug('display.display rc: {}'.format(return_code))
+            LOGGER.debug('display.display rc: {}'.format(return_code))
             image = Image.open('icons/camera_icon.ppm').resize((128,64), Image.ANTIALIAS).convert('1')
-            logger.debug('image creation from local icons folder: {}'.format(image))
+            LOGGER.debug('image creation from local icons folder: {}'.format(image))
             return_code = self.display.image(image)
-            logger.debug('display.image rc: {}'.format(return_code))
+            LOGGER.debug('display.image rc: {}'.format(return_code))
             return_code = self.display.display()
-            logger.debug('display.display rc: {}'.format(return_code))
+            LOGGER.debug('display.display rc: {}'.format(return_code))
         elif command == "time":
-            logger.debug("Handling time...")
+            LOGGER.debug("Handling time...")
             return_code = self.display.clear()
-            logger.debug('display.clear rc: {}'.format(return_code))
+            LOGGER.debug('display.clear rc: {}'.format(return_code))
             return_code = self.display.display()
-            logger.debug('display.clear rc: {}'.format(return_code))          
+            LOGGER.debug('display.clear rc: {}'.format(return_code))          
 
             im = Image.new('1',(128,64))
-            logger.debug('image creation: {}'.format(im))
+            LOGGER.debug('image creation: {}'.format(im))
             draw = ImageDraw.Draw(im)
             return_code = draw.text((0,0), clean_msg[1], font=self.font, fill=255)
-            logger.debug('draw results: {}'.format(return_code))
+            LOGGER.debug('draw results: {}'.format(return_code))
             return_code = self.display.image(im)
-            logger.debug('display.image rc: {}'.format(return_code))
+            LOGGER.debug('display.image rc: {}'.format(return_code))
             return_code = self.display.display()
-            logger.debug('display.display rc: {}'.format(return_code))
+            LOGGER.debug('display.display rc: {}'.format(return_code))
         elif command == "clear":
             return_code = self.display.clear()
-            logger.debug('display.clear rc: {}'.format(return_code))
+            LOGGER.debug('display.clear rc: {}'.format(return_code))
             return_code = self.display.display()
-            logger.debug('display.display rc: {}'.format(return_code))
+            LOGGER.debug('display.display rc: {}'.format(return_code))
 
 if __name__ == "__main__":
     DISPLAY = Display()
